ab9c339c0b9381ef4bdc0d9314c69bb3b47c3371
acherrera <anthonyherrera24@gmail.com>
2017-01-31 12:10:20 -0600

    added tutorial 4

--- 4_quit_event.py
+++ 4_quit_event.py
@@ -1,6 +1,9 @@
 """
 Learning how to handle events - Inputs
 Making a 'snake' game copy
+2 - making a basic framework - crappy framework
+3 - showing what events look like
+4 - adding quit function to program - good framework
 """
 import pygame
 
@@ -16,8 +19,8 @@ gameExit = False
 # Basic game loop goes here. Keep the window around
 while not gameExit:
     for event in pygame.event.get():
-        print(event)
-# TODO Put quit function here
+        if event.type == pygame.QUIT:  # This will exit out
+            gameExit = True
 
 pygame.quit()
 quit()
