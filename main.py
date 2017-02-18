a41592047f2fa8ad69bc78257063cfaa1663f380
Anthony <anthonyherrera@gmail.com>
2017-02-18 13:29:24 -0600

    Added missing folders to github repo

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+""" Use this in main.py to get the relative path for bg image
+import os
+dir = os.path.dirname(__file__)
+print(dir)
+bgPath = os.path.join(dir, '/resource/background.png')
+"""
